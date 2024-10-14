ad053368106e352994839cd92df151b3648c6246
manindra <manindratoyou@gmail.com>
2024-10-14 16:36:28 +0530

    Initial commit

---
+++ Base-Domains/src/test/java/com/manindra/Base_Domains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manindra.Base_Domains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
